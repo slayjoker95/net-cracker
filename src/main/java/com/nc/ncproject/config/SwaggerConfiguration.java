9018425831a08aaeec5763e1229a43873fe43f27
eziyangulov <eduard.ziyangulov@netcracker.com>
2017-10-30 15:06:37 +0300

    Swagger + Rest Controller

--- src/main/java/com/nc/ncproject/config/SwaggerConfiguration.java
+++ src/main/java/com/nc/ncproject/config/SwaggerConfiguration.java
@@ -1,6 +1,10 @@
 package com.nc.ncproject.config;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -8,9 +12,6 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static springfox.documentation.builders.PathSelectors.regex;
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 
 @Configuration("LaptopsSwaggerConfiguration")
 @EnableSwagger2
@@ -20,8 +21,8 @@ public class SwaggerConfiguration {
     public Docket laptopsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(basePackage("com.ncproject"))
-                .paths(regex("/api/laptops.*"))
+                .apis(basePackage("com.nc.ncproject"))
+                .paths(regex("/api/.*"))
                 .build()
                 .groupName("laptops")
                 .useDefaultResponseMessages(false)
@@ -31,7 +32,7 @@ public class SwaggerConfiguration {
     private ApiInfo getApiInfo() {
         return new ApiInfoBuilder()
                 .title("Laptops REST API")
-                .description("This API allows to perform all operations related to lead management")
+                .description("This API allows to perform all operations related to laptops")
                 .version("1.0")
                 .build();
     }
--- src/main/java/com/nc/ncproject/controller/LaptopRestController.java
+++ src/main/java/com/nc/ncproject/controller/LaptopRestController.java
@@ -1,7 +1,7 @@
 package com.nc.ncproject.controller;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 import java.util.List;
 
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,4 +39,26 @@ public class LaptopRestController {
     	Laptop laptop = laptopService.getByID(id);
     	return new ResponseEntity<Laptop>(laptop, HttpStatus.OK);
     }
+
+    @ApiOperation(value = "Create a laptop", produces = APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(method = POST)
+    public ResponseEntity<Laptop> createLaptop(@RequestBody Laptop laptop) {
+    	laptopService.add(laptop);
+        return new ResponseEntity<Laptop>(laptop, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Update a laptop", produces = APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/{id}", method = PUT)
+    public ResponseEntity<Laptop> updateLaptop(@RequestBody Laptop laptop) {
+    	laptopService.update(laptop);
+        return new ResponseEntity<Laptop>(laptop, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Delete a laptop", produces = APPLICATION_JSON_UTF8_VALUE)
+    @RequestMapping(value = "/{id}", method = DELETE)
+    public ResponseEntity<Laptop> deleteLaptop(@ApiParam(value = "Laptop id", required = true) @PathVariable String id) {
+    	laptopService.delete(id);
+        return new ResponseEntity<Laptop>(HttpStatus.OK);
+    }
+
 }
